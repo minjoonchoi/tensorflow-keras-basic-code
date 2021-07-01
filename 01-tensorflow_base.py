f7c32104158602866167eb90388cdcbd16c97517
minjoon.choi <1226choi@gmail.com>
2021-07-02 02:46:28 +0900

    스크립트 사용 방법 변경

--- 01-tensorflow_base.py
+++ 01-tensorflow_base.py
@@ -1,3 +1,6 @@
+import sys
+import argparse
+
 import timeit
 import numpy as np
 import tensorflow as tf
@@ -12,10 +15,8 @@ if tf.__version__ < "2":
 else:
     print('Tensorflow version 2.x supports Eager exeuction')
 
-def check_tensor_contant():
-    """텐서 상수 확인
-    """
-    # 텐서 상수
+def tensor_contant():
+    # Tensor 상수
     a = tf.constant(2)
     print('Rank(차원) : ', tf.rank(a))
     print('Value(텐서) : ', a)
@@ -32,9 +33,7 @@ def check_tensor_contant():
     print('Rank(차원) : ', tf.rank(d))
     print('Value(텐서) : ', d)
 
-def check_random_tensor():
-    """random 텐서 확인
-    """
+def random_tensor():
     # uniform 난수 생성
     rand_uniform = tf.random.uniform(shape=(10,), minval=0, maxval=1)
     print('rand_uniform.shape : ',rand_uniform.shape)
@@ -45,9 +44,7 @@ def check_random_tensor():
     print('rand_normal.shape : ',rand_normal.shape)
     print('rand_normal : ', rand_normal)
 
-def check_eager_execution():
-    """Eager execution 확인 : 세션을 초기화 없이 그래프를 즉시 실행
-    """
+def eager_execution():
     a = tf.constant(5)
     b = tf.constant(2)
     print('tf.add(a,b) : ', tf.add(a,b))
@@ -57,9 +54,7 @@ def check_eager_execution():
     print('tf.multiply(a,b) : ',tf.multiply(a,b))
     print('a*b : ',a*b)
 
-def check_tensor_and_numpy():
-    """Tensor와 Numpy 배열 호환성 확인
-    """
+def tensor_and_numpy():
     a = tf.constant(5)
     b = tf.constant(2)
     np_value = tf.add(a, b)
@@ -74,17 +69,13 @@ def check_tensor_and_numpy():
     print(type(square_tensor))
     print(square_tensor)
 
-def check_slicing_tensor():
-    """텐서 슬라이싱 확인
-    """
+def slicing_tensor():
     tensor = tf.constant([[1., 3., 5.],[7., 9., 11.]])
     print(tensor)
     print(tensor[:, 1:])
     print(tensor[..., 1, tf.newaxis])
 
-def check_tensor_function():
-    """텐서 지원 함수 확인
-    """
+def tensor_function():
     tensor = tf.constant([[1., 3., 5., 7.],[7., 9., 11., 13.]])
     print(tensor + 1)
     print(tf.square(tensor))
@@ -92,9 +83,7 @@ def check_tensor_function():
     print(tf.matmul(tensor, tf.transpose(tensor)))
     print(tf.cast(tensor, dtype=tf.int32))
 
-def check_function_with_tf_function_annot():
-    """tf.function 적용 유무에 따른 함수 수행시간 비교
-    """
+def function_with_tf_function_annot():
 
     @tf.function
     def tf_fun(x):
@@ -112,9 +101,7 @@ def check_function_with_tf_function_annot():
     print('함수 Graph time ', timeit.timeit(lambda: fun(inputs), number=10000))
 
 
-def check_model_with_tf_function():
-    """tf.function 적용 유무에 따른 Keras 모델 학습 시간 비교
-    """
+def model_with_tf_function():
     inputs = tf.random.normal((120, 28, 28))
 
     eager_model = Sequential()
@@ -129,9 +116,7 @@ def check_model_with_tf_function():
     print('클래스 Graph time ', timeit.timeit(lambda: graph_model(inputs), number=10000))
 
 
-def check_graph_gradient():
-    """미분 연산 확인
-    """
+def graph_gradient():
     W = tf.Variable(tf.random.normal((3,2)), name='weights')
     B = tf.Variable(tf.zeros(2, dtype=tf.float32), name='biases')
 
@@ -146,32 +131,46 @@ def check_graph_gradient():
         print(B.shape)
         print(dl_db.shape)
 
-def main():
 
-    # 텐서 상수
-    check_tensor_contant()
+#----------------------------------------------------------------------------
 
-    # 랜덤 텐서
-    check_random_tensor()
+def execute_cmdline(argv):
+    prog = argv[0]
+    parser = argparse.ArgumentParser(
+        prog        = prog,
+        description = 'Tensorflow 기본 예제 코드',
+        epilog      = 'Type "%s <command> -h" for more information.' % prog)
 
-    # 즉시실행 모드
-    check_eager_execution()
+    subparsers = parser.add_subparsers(dest='command')
+    subparsers.required = True
+    def add_command(cmd, desc, example=None):
+        epilog = 'Example: %s %s' % (prog, example) if example is not None else None
+        return subparsers.add_parser(cmd, description=desc, help=desc, epilog=epilog)
 
-    # Numpy 호환성 확인
-    check_tensor_and_numpy()
+    p = add_command(cmd='tensor_contant', desc='tf.constant Tensor')
+    
+    p = add_command(cmd='random_tensor',  desc='tf.random Tensor')
+    
+    p = add_command(cmd='eager_execution',  desc='Eager execution : 세션을 초기화 없이 그래프를 즉시 실행')
+    
+    p = add_command(cmd='tensor_and_numpy',  desc='Tensor와 Numpy 배열 호환성')
 
-    # 텐서 슬라이싱
-    check_slicing_tensor()
+    p = add_command(cmd='slicing_tensor',  desc='Tensor 슬라이싱')
 
-    # 텐서 지원 함수 
-    check_tensor_function()
+    p = add_command(cmd='tensor_function',  desc='Tensor 지원 함수 예제')
 
-    # tf.function
-    check_function_with_tf_function_annot()
-    check_model_with_tf_function()
+    p = add_command(cmd='function_with_tf_function_annot',  desc='tf.function 유무에 따른 함수 수행시간 비교')
 
-    # gradient(미분) 값
-    check_graph_gradient()
+    p = add_command(cmd='model_with_tf_function',  desc='tf.function 유무에 따른 Keras 모델 학습 시간 비교')
 
-if __name__ == '__main__':
-    main()
+    p = add_command(cmd='graph_gradient',  desc='미분 연산')
+    
+    args = parser.parse_args(argv[1:] if len(argv) > 1 else ['-h'])
+    func = globals()[args.command]
+    del args.command
+    func(**vars(args))
+
+#----------------------------------------------------------------------------
+
+if __name__ == "__main__":
+    execute_cmdline(sys.argv)
